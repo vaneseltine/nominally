160ccffc938667f7517e8f218551de73b8c87d95
Matt VanEseltine <matvan@umich.edu>
2019-09-03 22:25:01 -0400

    Introduce clean; update testing

--- nominally/util.py
+++ nominally/util.py
@@ -1,4 +1,7 @@
 import logging
+import re
+
+from unidecode import unidecode_expect_ascii
 
 log = logging.getLogger("HumanName")
 log.addHandler(logging.NullHandler())
@@ -20,3 +23,18 @@ def lc(value):
     if not value:
         return ""
     return value.lower().strip(".")
+
+
+def clean(
+    s,
+    lowercase=True,
+    replace_with_space=r"[\s/\\_]+",
+    replace_with_none=r"[^ \-A-Za-z'\"()]+",
+):
+    if not isinstance(s, str):
+        return ""
+    s = unidecode_expect_ascii(s)
+    s = re.sub(replace_with_space, " ", s)
+    s = re.sub(replace_with_none, "", s)
+    s = s.lower().strip()
+    return s
---
+++ test/util_test.py
@@ -0,0 +1,88 @@
+import pytest
+
+from nominally.util import clean
+
+
+class TestClean:
+    @pytest.mark.parametrize(
+        "raw", ["DINSDALE", "Dinsdale", "dINSDALE", "dinsdale", "DiNsDaLe"]
+    )
+    def t_lower(self, raw):
+        assert clean(raw) == "dinsdale"
+
+    @pytest.mark.parametrize(
+        "raw",
+        [
+            " ❤  Spiny Norman ",
+            "Spiny Norman != 🐈 ",
+            "Spiny Norman != 🐱 ",
+            "Spiny Norman == 🐾 ",
+        ],
+    )
+    def t_emoji(self, raw):
+        assert clean(raw) == "spiny norman"
+
+    @pytest.mark.parametrize("raw", ["Mr Εric Πραλiñé"])
+    def t_convert_unicode(self, raw):
+        """
+        This is handled by unidecode and does not need to be extensively tested
+        """
+        assert clean(raw) == "mr eric praline"
+
+    @pytest.mark.parametrize(
+        "raw",
+        [
+            "Mr Eric Praline",
+            "Mr Eric Praline ",
+            " Mr Eric Praline",
+            "Mr  Eric Praline",
+            "Mr   Eric   Praline",
+            "  Mr  Eric  Praline  ",
+            "Mr Eric Praline \t",
+            " \n Mr Eric Praline",
+            "Mr Eric Praline \r",
+            "Mr \r\n Eric Praline ",
+        ],
+    )
+    def t_drop_spacing(self, raw):
+        assert clean(raw) == "mr eric praline"
+
+    @pytest.mark.parametrize(
+        "raw",
+        [
+            r"!M!r Eri!c Praline!!",
+            r"//Mr Eric Praline",
+            r"M2r Er4ic Pra32lin5e",
+            r"Mr Eric Praline_____",
+            r"Mr Eric Praline",
+            r"Mr Eric/Praline",
+            r"Mr_Eric_Praline",
+            r"Mr% Eric Pr#ali&ne",
+        ],
+    )
+    def t_ignore_most_symbols(self, raw):
+        assert clean(raw) == "mr eric praline"
+
+    @pytest.mark.parametrize(
+        "raw, cooked",
+        [
+            ("Dinsdale", "dinsdale"),
+            ("(Dinsdale)", "(dinsdale)"),
+            ("-Dinsdale-", "-dinsdale-"),
+            ("'Dins'dale'", "'dins'dale'"),
+        ],
+    )
+    def t_keep_certain_symbols(self, raw, cooked):
+        assert clean(raw) == cooked
+
+    @pytest.mark.parametrize(
+        "raw, cooked",
+        [
+            ("Dinsdale-", "dinsdale"),
+            ("Dinsdale--", "dinsdale"),
+            ("Dinsdale--", "dinsdale"),
+        ],
+    )
+    @pytest.mark.xfail(reason="Unimplemented")
+    def t_strip_margin_symbols(self, raw, cooked):
+        assert clean(raw) == cooked
