914e3f4a8b973958596b759d1d1fafdd996a20a9
Matt VanEseltine <matvan@umich.edu>
2019-09-08 00:47:00 -0400

    Workflow update

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from pathlib import Path
 
 import setuptools
 
+import nominally
+
 setuptools.setup(
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": [f"nominally = nominally:cli_parse"]},
     install_requires=Path("./requirements/common.txt").read_text(),
+    version=nominally.__version__,
 )
