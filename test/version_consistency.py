e499455ece0fe20acfbe84e4f292ab5aeb86b417
Matt VanEseltine <matvan@umich.edu>
2019-09-08 22:43:37 -0400

    Bump version, though primarily due to CI improvement

--- test/version_consistency.py
+++ test/version_consistency.py
@@ -2,27 +2,7 @@ import re
 import subprocess
 from pathlib import Path
 
-VERSION_PATTERN = re.compile(r"version.*?(\d+\.\d+\.\d+)")
-
-
-def get_tagged():
-    result = subprocess.run(
-        ["git", "describe", "--tags", "--abbrev=0"], stdout=subprocess.PIPE
-    )
-    return result.stdout.decode("utf-8").strip()
-
-
-def read_n_grep(path, pattern):
-    text = path.read_text()
-    return pattern.search(text).group(1)
-
-
-def get_module():
-    return read_n_grep(Path("./nominally/__init__.py"), VERSION_PATTERN)
-
-
-def get_setup_cfg():
-    return read_n_grep(Path("./setup.cfg"), VERSION_PATTERN)
+VERSION_PATTERN = r"(\d+\.\d+\.\d+)"
 
 
 def main():
@@ -32,7 +12,37 @@ def main():
         "setup.cfg": get_setup_cfg(),
     }
     if len(set(versions.values())) != 1:
-        raise AssertionError(f"Inconsistent versions: f{versions}")
+        from pprint import pprint
+
+        pprint(versions)
+        raise AssertionError(f"Version problem: f{versions}")
+
+
+def get_tagged():
+    result = subprocess.run(
+        ["git", "describe", "--tags", "--abbrev=0"], stdout=subprocess.PIPE
+    )
+    return result.stdout.decode("utf-8").strip()
+
+
+def get_module():
+    path = Path("./nominally/__init__.py")
+    prefix = '__version__[ ="]+?'
+    return read_n_grep(path, prefix)
+
+
+def get_setup_cfg():
+    path = Path("./setup.cfg")
+    prefix = "version[ =]+?"
+    return read_n_grep(path, prefix)
+
+
+def read_n_grep(path, prefix, pattern=VERSION_PATTERN):
+    text = Path(path).read_text()
+    result = re.compile(prefix + pattern).search(text)
+    if not result:
+        return f"Failed to find version in {path}"
+    return result.group(1)
 
 
 if __name__ == "__main__":
