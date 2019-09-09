0e0745736121212f56fe008fce6249cd633212e4
Matt VanEseltine <matvan@umich.edu>
2019-09-08 23:23:58 -0400

    Updating issues tracking

--- test/test__issues.py
+++ test/test__issues.py
@@ -9,7 +9,7 @@ from .conftest import dict_entry_test
     raises=AssertionError,
     reason="Allow multiword single quote nicknames while still supporting apostrophes",
 )
-def allow_outer_only_single_quotes():
+def issue_3_allow_outer_only_single_quotes():
     name = Name("Gwinifer 'Old Mother' Blackcap")
     assert name.nickname == "old mother"
 
@@ -18,7 +18,7 @@ def allow_outer_only_single_quotes():
     raises=AssertionError,
     reason="Clean each nickname exactly like post-nickname full names",
 )
-def test_clean_nicknames():
+def issue_4_clean_nicknames():
     name = Name("ang%ua (ba%%rky) uberwald")
     assert name.first == "angua"
     assert name.nickname == "barky"
@@ -28,7 +28,7 @@ def test_clean_nicknames():
     raises=AssertionError,
     reason="Probably not, but add a placeholder to keep names separated",
 )
-def test_leave_non_comma_placeholder_from_nicknames():
+def issue_5_leave_non_comma_placeholder_from_nicknames():
     name = Name("angua van (barky) uberwald")
     assert name.last == "uberwald"
 
@@ -37,7 +37,7 @@ def test_leave_non_comma_placeholder_from_nicknames():
     raises=AttributeError,
     reason="Initiate new Name by passing an existing instance of Name",
 )
-def test_name_to_name():
+def issue_6_name_to_name():
     name1 = Name("Dr. Horace 'Ook' Worblehat")
     name2 = Name(name1)
     assert name1 == name2
@@ -45,18 +45,9 @@ def test_name_to_name():
 
 @pytest.mark.xfail(
     raises=AssertionError,
-    reason="Probably accept that we can't have 'Ms. von Uberwald' and Ms. Van Eseltine",
-)
-def test_first_name_is_prefix_if_three_parts():
-    n = Name("Ms. von Uberwald")
-    assert n.last == "von uberwald"
-
-
-@pytest.mark.xfail(
-    raises=AssertionError,
     reason="Add nuance to Ph.D. and other inital suffixes to support, e.g., J.R.R.",
 )
-def test_allow_initials_written_properly():
+def issue_7_allow_initials_written_properly():
     n = Name("J.R.R. Tolkien")
     assert n.first == "j"
     assert n.middle == "r r"
@@ -74,5 +65,5 @@ def test_allow_initials_written_properly():
     raises=AssertionError,
     reason="Find some good ways to make suffixes when initials are unlikely.",
 )
-def test_distinguish_suffixes_suitably_distinguishable_from_initials(entry):
+def issue_8_distinguish_suffixes_suitably_distinguishable_from_initials(entry):
     dict_entry_test(Name, entry)
