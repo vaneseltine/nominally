c80cc8d31345192917cb33428b25866298d259da
Matt VanEseltine <matvan@umich.edu>
2019-09-20 23:48:53 -0400

    Add fingerprint testing, close #30

--- test/test_name_blasting.py
+++ test/test_name_blasting.py
@@ -15,3 +15,16 @@ def test_idempotence(entry):
     name = Name(entry["raw"])
     namename = Name(str(name))
     assert name == namename
+
+
+@pytest.mark.parametrize("entry", load_bank("*"), ids=make_ids)
+def issue_30_check_fingerprint(entry):
+    name = Name(entry["raw"])
+    rawish = name._pre_clean(entry["raw"])
+    if "junior" in rawish:
+        return
+    assert fingerprint(rawish) == fingerprint(str(name))
+
+
+def fingerprint(s):
+    return "".join(sorted(x for x in s if x.isalpha()))
